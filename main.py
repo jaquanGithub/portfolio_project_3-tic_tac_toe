72d4c8fb53271f5c7d12e8fe03af8783bf495ae7
yudel <jaquan.code@gmail.com>
2026-02-24 22:27:17 +0200

    Change board layout

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-tiles = [['0', '0', '0'],
-         ['0', '0', '0'],
-         ['X', '0', '0']]
+import pandas as pd
 
-board = f'''   {tiles[0][0]} | {tiles[0][1]} | {tiles[0][2]}
----------------
-   {tiles[1][0]} | {tiles[1][1]} | {tiles[1][2]}
----------------
-   {tiles[2][0]} | {tiles[2][1]} | {tiles[2][2]}'''
+tiles = [['O', 'O', 'O'],
+         ['O', 'O', 'O'],
+         ['O', 'O', 'O']]
+
+board = f'''    A | B | C\n
+1   {tiles[0][0]} | {tiles[0][1]} | {tiles[0][2]}
+{"-"*15}
+2   {tiles[1][0]} | {tiles[1][1]} | {tiles[1][2]}
+{"-"*15}
+3   {tiles[2][0]} | {tiles[2][1]} | {tiles[2][2]}'''
 
 print(board)
